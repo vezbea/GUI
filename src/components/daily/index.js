bddbcaec7aa4bdf145e72899a1c211bbf5b64039
Verity Beatrice Grant <vg301@itl095.student.eecs.qmul.ac.uk>
2017-03-08 17:15:07 +0000

    Changed what it shown in weekly and daily, added some icons and changed the design slightly

--- src/components/daily/index.js
+++ src/components/daily/index.js
@@ -19,13 +19,23 @@ export default class Daily extends Component {
         var hr6 = parsed_json['hourly_forecast'][5]['FCTTIME']['hour_padded'];
         var hr7 = parsed_json['hourly_forecast'][6]['FCTTIME']['hour_padded'];
 
-        var temp1 = parsed_json['hourly_forecast'][0]['temp']['metric'];
-        var temp2 = parsed_json['hourly_forecast'][1]['temp']['metric'];
-        var temp3 = parsed_json['hourly_forecast'][2]['temp']['metric'];
-        var temp4 = parsed_json['hourly_forecast'][3]['temp']['metric'];
-        var temp5 = parsed_json['hourly_forecast'][4]['temp']['metric'];
-        var temp6 = parsed_json['hourly_forecast'][5]['temp']['metric'];
-        var temp7 = parsed_json['hourly_forecast'][6]['temp']['metric'];
+        var cover1 = parsed_json['hourly_forecast'][0]['sky'];
+        var cover2 = parsed_json['hourly_forecast'][1]['sky'];
+        var cover3 = parsed_json['hourly_forecast'][2]['sky'];
+        var cover4 = parsed_json['hourly_forecast'][3]['sky'];
+        var cover5 = parsed_json['hourly_forecast'][4]['sky'];
+        var cover6 = parsed_json['hourly_forecast'][5]['sky'];
+        var cover7 = parsed_json['hourly_forecast'][6]['sky'];
+
+        var rain1 = parsed_json['hourly_forecast'][0]['pop'];
+        var rain2 = parsed_json['hourly_forecast'][1]['pop'];
+        var rain3 = parsed_json['hourly_forecast'][2]['pop'];
+        var rain4 = parsed_json['hourly_forecast'][3]['pop'];
+        var rain5 = parsed_json['hourly_forecast'][4]['pop'];
+        var rain6 = parsed_json['hourly_forecast'][5]['pop'];
+        var rain7 = parsed_json['hourly_forecast'][6]['pop'];
+
+
 
     		console.log("Success");
     		// set the states for fields so they could be rendered later on with all the datas saved into states.
@@ -38,18 +48,26 @@ export default class Daily extends Component {
               hour6 : hr6,
               hour7 : hr7,
 
-              t1 : temp1,
-              t2 : temp2,
-              t3 : temp3,
-              t4 : temp4,
-              t5 : temp5,
-              t6 : temp6,
-              t7 : temp7,
+              cloud1 : cover1,
+              cloud2 : cover2,
+              cloud3 : cover3,
+              cloud4 : cover4,
+              cloud5 : cover5,
+              cloud6 : cover6,
+              cloud7 : cover7,
+
+              pop1 : rain1,
+              pop2 : rain2,
+              pop3 : rain3,
+              pop4 : rain4,
+              pop5 : rain5,
+              pop6 : rain6,
+              pop7 : rain7,
     		});
     }
 
     componentWillMount = () => {
-        var url = "http://api.wunderground.com/api/7785277273971137/hourly/q/UK/London.json";
+        var url = "http://api.wunderground.com/api/e882807beab49ff4/hourly/q/UK/London.json";
         //fetch API
         $.ajax({
                     url: url,
@@ -64,6 +82,8 @@ export default class Daily extends Component {
             <div class = {style.daily}>
             <table class = {style.dailyTable}>
                 <tr>
+                    <td><img src="https://cdn0.iconfinder.com/data/icons/feather/96/clock-128.png" class = {style.cloud}/></td>
+
                     <th> {this.state.hour1}:00 </th>
                     <th> {this.state.hour2}:00 </th>
                     <th> {this.state.hour3}:00 </th>
@@ -73,13 +93,24 @@ export default class Daily extends Component {
                     <th> {this.state.hour7}:00 </th>
                 </tr>
                 <tr>
-                    <td> {this.state.t1}°C </td>
-                    <td> {this.state.t2}°C </td>
-                    <td> {this.state.t3}°C </td>
-                    <td> {this.state.t4}°C </td>
-                    <td> {this.state.t5}°C </td>
-                    <td> {this.state.t6}°C </td>
-                    <td> {this.state.t7}°C </td>
+                    <td><img src="http://simpleicon.com/wp-content/uploads/cloud-10.png" class = {style.cloud}/></td>
+                    <td> {this.state.cloud1}% </td>
+                    <td> {this.state.cloud2}% </td>
+                    <td> {this.state.cloud3}% </td>
+                    <td> {this.state.cloud4}% </td>
+                    <td> {this.state.cloud5}% </td>
+                    <td> {this.state.cloud6}% </td>
+                    <td> {this.state.cloud7}% </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td> {this.state.pop1>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop2>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop3>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop4>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop5>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop6>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
+                    <td> {this.state.pop7>=50 && <img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.imagesD}/>} </td>
                 </tr>
             </table>
             </div>
--- src/components/weekly/index.js
+++ src/components/weekly/index.js
@@ -33,6 +33,14 @@ export default class Weekly extends Component {
         var low6 = parsed_json['forecast']['simpleforecast']['forecastday'][5]['low']['celsius'];
         var low7 = parsed_json['forecast']['simpleforecast']['forecastday'][6]['low']['celsius'];
 
+        var cond1 = parsed_json['forecast']['simpleforecast']['forecastday'][0]['conditions'];
+        var cond2 = parsed_json['forecast']['simpleforecast']['forecastday'][1]['conditions'];
+        var cond3 = parsed_json['forecast']['simpleforecast']['forecastday'][2]['conditions'];
+        var cond4 = parsed_json['forecast']['simpleforecast']['forecastday'][3]['conditions'];
+        var cond5 = parsed_json['forecast']['simpleforecast']['forecastday'][4]['conditions'];
+        var cond6 = parsed_json['forecast']['simpleforecast']['forecastday'][5]['conditions'];
+        var cond7 = parsed_json['forecast']['simpleforecast']['forecastday'][6]['conditions'];
+
         var rain1 = parsed_json['forecast']['simpleforecast']['forecastday'][0]['pop'];
         var rain2 = parsed_json['forecast']['simpleforecast']['forecastday'][1]['pop'];
         var rain3 = parsed_json['forecast']['simpleforecast']['forecastday'][2]['pop'];
@@ -68,6 +76,14 @@ export default class Weekly extends Component {
               l6 : low6,
               l7 : low7,
 
+              con1 : cond1,
+              con2 : cond2,
+              con3 : cond3,
+              con4 : cond4,
+              con5 : cond5,
+              con6 : cond6,
+              con7 : cond7,
+
               pop1 : rain1,
               pop2 : rain2,
               pop3 : rain3,
@@ -79,7 +95,7 @@ export default class Weekly extends Component {
     }
 
     componentWillMount = () => {
-        var url = "http://api.wunderground.com/api/7785277273971137/forecast10day/q/UK/London.json";
+        var url = "http://api.wunderground.com/api/e882807beab49ff4/forecast10day/q/UK/London.json";
         //fetch API
         $.ajax({
                     url: url,
@@ -93,39 +109,53 @@ export default class Weekly extends Component {
         return (
             <div class = {style.weekly}>
                 <table class = {style.weeklyTable}>
+                    <tr>
+                        <th></th>
+                        <th><img src="https://cdn3.iconfinder.com/data/icons/weather-16/256/Hot_Day-512.png" class = {style.images}/></th>
+                        <th>Condition</th>
+                        <th><img src="http://www.morningrefresh.com/images/dynamic/201411/1417016079D2x.png" class = {style.images}/></th>
+                    </tr>
+
                     <tr class = {style.topRow}>
-                        <th>{this.state.d1}</th>
+                        <th>Today</th>
                         <td>{this.state.l1}°C - {this.state.h1}°C</td>
+                        <td>{this.state.con1}</td>
                         <td> {this.state.pop1}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d2}</th>
                         <td>{this.state.l2}°C - {this.state.h2}°C</td>
+                        <td>{this.state.con2}</td>
                         <td> {this.state.pop2}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d3}</th>
                         <td>{this.state.l3}°C - {this.state.h3}°C</td>
+                        <td>{this.state.con3}</td>
                         <td> {this.state.pop3}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d4}</th>
                         <td>{this.state.l4}°C - {this.state.h4}°C</td>
+                        <td>{this.state.con4}</td>
                         <td> {this.state.pop4}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d5}</th>
                         <td>{this.state.l5}°C - {this.state.h5}°C</td>
+                        <td>{this.state.con5}</td>
                         <td> {this.state.pop5}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d6}</th>
                         <td>{this.state.l6}°C - {this.state.h6}°C</td>
+                        <td>{this.state.con6}</td>
                         <td> {this.state.pop6}% </td>
                     </tr>
                     <tr>
                         <th>{this.state.d7}</th>
                         <td>{this.state.l7}°C - {this.state.h7}°C</td>
+                        <td>{this.state.con7}</td>
                         <td> {this.state.pop7}% </td>
                     </tr>
                 </table>
